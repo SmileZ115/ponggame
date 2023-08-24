1fee37beca5a76b8efc3153ba8fe5e1a8a018a68
Arthur <anwcrazy@outlook.com>
2023-08-24 10:29:46 -0300

    last update

--- pong/Assets/Scripts/Ball.cs
+++ pong/Assets/Scripts/Ball.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public float Speed = 5;
+    public float Speed = 5f;
     void Start()
     {
         float SpeedX = Random.Range(0, 2) == 0 ? -1 : 1;
         float SpeedY = Random.Range(0, 2) == 0 ? -1 : 1;
 
-        GetComponent<Rigidbody>().velocity = new Vector3(Speed * SpeedX, Speed * SpeedY, 0);
+        GetComponent<Rigidbody>().velocity = new Vector3(Speed * SpeedX, Speed * SpeedY, 0f);
     }
     void Update()
     {
